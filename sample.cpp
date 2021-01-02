918362c221ee188b1bc6b8b73c35f378a65efcbb
sameernegi17 <sameer.negi17@gmail.com>
2021-01-02 12:05:13 +0100

    C++ Introduction

---
+++ sample.cpp
@@ -0,0 +1,27 @@
+/*
+Primitive Type :
+Interger : 42,64
+character : a,b,c
+boolean : true, false
+float : 42.24f
+double : 52.6484
+void
+*/
+
+/*
+User Defined Type 
+/*
+std::string : sameer
+std::vector : [1,2,3,4,5,.....]
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    int x = 42;
+    char a = 'c';
+    std::cout << x << " " << a << std::endl;
+    return 0;
+}
